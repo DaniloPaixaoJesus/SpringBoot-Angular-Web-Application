02628cc7636b54d8e22a6ff2eac878099261f327
DaniloPaixaoJesus <contato.danilo.paixao@gmail.com>
2019-01-30 14:04:53 -0200

    deploy ec2 aws

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
-  apiUrl: "ec2-34-229-135-40.compute-1.amazonaws.com:3000"
+  apiUrl: "http://ec2-34-229-135-40.compute-1.amazonaws.com:3000"
 };
